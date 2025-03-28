a75112ae04f64cedd4c79826fb439d8c12989add
yogeshjat23 <22bec116@nith.ac.in>
2025-03-29 01:49:17 +0530

    fil

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin: ['http://localhost:3000', 'https://athletink-bak.onrender.com'],
+
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+ }));
+
 app.use(express.json());  
 
 
